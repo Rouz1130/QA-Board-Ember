f169ddabdfc31deed8e2ee87202f36512ad27980
rouz majlessi <rmajlessi@gmail.com>
2016-09-14 13:48:24 -0700

    edit answer file add actions for it

--- app/components/edit-answer.js
+++ app/components/edit-answer.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  editAnswerForm: false,
+  actions: {
+    editAnswerForm() {
+      this.set('editAnswerForm', true);
+    },
+    editAnswer(answer) {
+      var params = {
+        author:this.get('author'),
+        text:this.get('text'),
+        question:this.get('question')
+      };
+      this.set('editAnswerForm', false);
+      this.sendAction('editAnswer', answer, params);
+    }
+  }
 });
